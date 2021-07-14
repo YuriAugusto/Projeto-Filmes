35139283eedd71dc1104b9c3bc6256474690a0ab
YuriAugusto <yuri.komesu@uni9.edu.br>
2021-07-14 15:42:45 -0300

    Parte 4 Aula 1

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -8,11 +8,13 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class CadastroFilmesComponent implements OnInit {
 
-  cadastro: FormGroup;
+  cadastro: FormGroup;//variável que representa o FormGroup dentro do html
 
   constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {//executa sempre que o componente for inicializado
+  //por padrão em angular caso a visibilidade de um método não seja definida ele será public
+  //em métodos não é necessário explicitar um retorno quando ele for void (vazio), porém é uma boa prática deixar o retorno void explícito
+  ngOnInit(): void {//executa sempre que o componente for inicializado
 
     this.cadastro = this.fb.group({//Validators permite que sejam aplicadas validações, já required é o método que torna o preenchimento do campo obrigatório
       titulo: ['',[Validators.required, Validators.minLength(2), Validators.maxLength(256)]],//aqui como 2º arg eu passo um array que representa todas as validações necessárias para serem aplicadas no campo
@@ -24,6 +26,18 @@ export class CadastroFilmesComponent implements OnInit {
       genero: ['', [Validators.required]]
     });
 
+  }
+
+  salvar(): void{
+    console.log("ENTROU");
+    if(this.cadastro.invalid){
+      return;//n retorna nada e impede o usuário de salvar
+    }
+    alert("SUCESSO!!\n\n" + JSON.stringify(this.cadastro.value, null, 4));
+  }
+
+  reiniciarForm(): void{//limpa todos os campos
+    this.cadastro.reset();
   }
 
 }
