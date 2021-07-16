4473006c6d5cac11c1961c410ac26e07ba88bd12
YuriAugusto <yuri.komesu@uni9.edu.br>
2021-07-16 09:29:43 -0300

    Parte 6 Aula 3

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Filme } from 'src/app/shared/models/filme';
 import { Component, OnInit } from '@angular/core';
 import { FilmesService } from 'src/app/core/filmes.service';
@@ -11,11 +12,21 @@ export class ListagemFilmesComponent implements OnInit {
 
   readonly qtdPagina = 4;//é uma variável que não pode ter seu valor alterado
   pagina = 0;
-  filmes: Filme[] = [];//defini inicializei o array filme como vazio
+  filmes: Filme[] = [];//defini e inicializei o array filme como vazio
+  filtrosListagem:FormGroup;
+  generos: Array<string>;
 
-  constructor(private filmesService: FilmesService) { }
+  constructor(private filmesService: FilmesService,
+    private fb: FormBuilder) { }
 
   ngOnInit():void {
+    this.filtrosListagem = this.fb.group({
+      texto: [''],//recebe um array vazio
+      genero: ['']
+    });
+
+    this.generos = ['Ação','Romance','Terror','Ficção científica','Comédia','Aventura','Drama'];
+
     this.listarFilmes();
   }
   onScroll(): void{
